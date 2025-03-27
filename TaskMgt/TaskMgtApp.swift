2481957bb827823882de4207498f056695268ffd
Richard Gagg <r.gagg@outlook.com>
2025-03-28 10:47:54 +1100

    Initial Commit

---
+++ TaskMgt/TaskMgtApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMgtApp.swift
+//  TaskMgt
+//
+//  Created by Richard Gagg on 28/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskMgtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
